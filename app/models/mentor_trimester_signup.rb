3834e5487db5f112103198de2d781d0116b3cd28
angel brown <angelkbrown@gmail.com>
2025-02-16 23:27:30 -0500

    Add trimester courses association, remove extra model

--- app/models/mentor_trimester_signup.rb
+++
@@ -1,4 +0,0 @@
-class MentorTrimesterSignup < ApplicationRecord
-  belongs_to :mentor
-  belongs_to :trimester
-end
--- app/models/trimester.rb
+++ app/models/trimester.rb
@@ -1,4 +1,6 @@
 class Trimester < ApplicationRecord
+  has_many :courses
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :application_deadline, presence: true
