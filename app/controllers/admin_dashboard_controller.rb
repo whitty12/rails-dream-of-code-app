a70e0bc7280ec15bd737aa364c4d35e066c66bfe
angel brown <angelkbrown@gmail.com>
2025-02-22 18:42:55 -0500

    Style and tidy

--- app/controllers/admin_dashboard_controller.rb
+++
@@ -1,3 +0,0 @@
-class AdminDashboardController < ApplicationController
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :submissions
   resources :students
   resources :mentors
   resources :enrollments
